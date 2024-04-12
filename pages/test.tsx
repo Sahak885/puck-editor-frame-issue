688142736040d031ece542336b39e360771197e3
Sahak <sahakzakaryan.work96@gmail.com>
2024-04-12 10:47:16 +0400

    Update

---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Test = () => {
+  return (
+    <div style={{display: "flex", alignItems: "center", justifyContent: "center", marginTop: "20px"}}>
+      <Link href="/">Hi Puck Editor</Link>
+    </div>
+  )
+}
+
+export default Test;
